b5341c75bb4bddef4e5fc99db2ceddbdd4a0e316
Tobias Baumann <tobbaumann@gmail.com>
2017-07-02 15:16:41 +0200

    add test

---
+++ src/test/java/de/baumato/loc/util/ThrowablesTest.java
@@ -0,0 +1,15 @@
+package de.baumato.loc.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class ThrowablesTest {
+
+	@Test
+	public void shouldConvertExceptionToString() throws Exception {
+		Exception e = new RuntimeException("message");
+		String stackTrace = Throwables.getStackTraceAsString(e);
+		assertThat(stackTrace).contains("RuntimeException", "message");
+	}
+}
