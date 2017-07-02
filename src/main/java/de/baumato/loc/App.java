9b3b1e2823305c09262a8ec5a175e68bc23b111c
Tobias Baumann <tobbaumann@gmail.com>
2017-07-02 21:46:08 +0200

    add verbose option

--- src/main/java/de/baumato/loc/App.java
+++ src/main/java/de/baumato/loc/App.java
@@ -18,8 +18,10 @@ public class App {
 	public static void main(String[] args) {
 		ConsolePrinter printer = new ConsolePrinter();
 		try {
+			Configuration conf = Configuration.ofCmdLine(args);
+			printer.setIsVerbose(conf.isVerbose());
 			printer.startProgress();
-			long numberOfLines = new LineCounter(Configuration.ofCmdLine(args)).count();
+			long numberOfLines = new LineCounter(conf, printer).count();
 			printer.done(numberOfLines);
 		} catch (InvalidCommandLineArgumentsException e) {
 			printer.doneWithError(e.getLocalizedMessage());
