9ab6dcd1c1041d3bba3408f1aaca5e25d1687001
Tobias Baumann <tobbaumann@gmail.com>
2017-07-22 23:44:30 +0200

    create executable lines-of-code.jsh during build
    This allows to use the program directly with lines-of-code.jsh instead
    of using java -jar lines-of-code.jar ...

--- create_executable_jar.sh
+++ create_executable_jar.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+set -e
+
+#jar_file=./target/lines-of-code-1.0.0-SNAPSHOT-shaded.jar
+jar_file=$1
+#exec_file_name="`basename $jar_file .jar`.jsh"
+#exec_file=./target/$exec_file_name
+exec_file=$2
+
+echo "jar file = $jar_file"
+echo "exec file = $exec_file"
+
 echo '#!/usr/bin/java -jar' > header.txt
-#new_file="`basename $1 .jar`.jsh"
-new_file=lines-of-code.jsh
-cat header.txt $1 > $new_file
-chmod +x $new_file
+cat header.txt $jar_file > $exec_file
+chmod +x $exec_file
 rm header.txt
-echo "successfully written " $new_file
+echo "successfully written " $exec_file
