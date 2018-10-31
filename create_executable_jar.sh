9b332de299218ccb9c01280dd6456d3c7d3279bb
Tobias Baumann <tobbaumann@gmail.com>
2018-10-31 20:03:35 +0100

    Use java on PATH.

--- create_executable_jar.sh
+++ create_executable_jar.sh
@@ -9,7 +9,7 @@ exec_file=$2
 echo "jar file = $jar_file"
 echo "exec file = $exec_file"
 
-echo '#!/usr/bin/java -jar' > header.txt
+echo '#!java -jar' > header.txt
 cat header.txt $jar_file > $exec_file
 chmod +x $exec_file
 rm header.txt
